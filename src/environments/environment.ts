0e5c76165e5ba66d88d84a6b42ecafcaf432185b
Asif Mulla <asifmulla166037@gmail.com>
2022-04-02 23:10:50 +0530

    multiple site hosting10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   BASE_URL:
     "https://vjpartners.co.in/ongoing/v7_new_clone_2/vj-sales-modules/public/api/",
 };
