978a07579e40a96fde93d5661056557ed1bd159c
Asif Mulla <asifmulla166037@gmail.com>
2022-04-03 07:29:44 +0530

    test ci_cd_fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   BASE_URL:
-    "https://vjpartners.co.in/ongoing/v7_new/vj-sales-modules/public/api/",
+    "https://vjpartners.co.in/ongoing/v7_new_clone_2/vj-sales-modules/public/api/",
 };
