cae378a3bf94da0cd71623462e6fc7bc51cf2d8a
Asif Mulla <asifmulla166037@gmail.com>
2022-04-07 20:10:31 +0530

    fixed2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  // BASE_URL:
-  // "https://vjpartners.co.in/ongoing/v7_new_clone_2/vj-sales-modules/public/api/",
-
   BASE_URL:
-    "https://vjpartners.co.in/ongoing/v7_new/vj-sales-modules/public/api/",
+    "https://vjpartners.co.in/ongoing/v7_new_clone_2/vj-sales-modules/public/api/",
+
+  // BASE_URL:
+  //   "https://vjpartners.co.in/ongoing/v7_new/vj-sales-modules/public/api/",
 };
