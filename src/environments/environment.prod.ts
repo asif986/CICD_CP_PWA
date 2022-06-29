501e6fd41a6acf9e234a14cc18d81fb17f78c38d
Asif Mulla <asifmulla166037@gmail.com>
2022-06-29 22:01:38 +0530

    latest changes with in pro team list module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: true,
+
+  //Test URL
   BASE_URL:
     "https://vjpartners.co.in/ongoing/v7_new_clone_2/vj-sales-modules/public/api/",
 
-  // live url
+  // live URL
   // BASE_URL:
   //   "https://vjpartners.co.in/ongoing/v7_new/vj-sales-modules/public/api/",
 };
